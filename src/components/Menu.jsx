d22656c4153a14f8bcc9c2e17cb19d9d83f9c463
Rachel <millerrach@users.noreply.github.com>
2019-01-22 21:09:07 +0100

    Animation without popmotion

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,19 +1,6 @@
 import React, { Component } from "react";
 import hamburger from "../images/hamburger.svg";
 import close from "../images/close.svg";
-import posed from "react-pose";
-import { tween } from "popmotion";
-
-const FullMenu = posed.div({
-  hide: {
-    height: 0,
-    transition: tween
-  },
-  show: {
-    height: "90vh",
-    transition: tween
-  }
-});
 
 class Menu extends Component {
   state = {
@@ -23,31 +10,35 @@ class Menu extends Component {
     this.setState({ closed: !this.state.closed });
   }
   render() {
+    let closed = this.state.closed;
     return (
       <div className="menuContainer" onClick={this.toggleMenu.bind(this)}>
-        {this.state.closed ? (
+        {closed ? (
           <div className="hamburgOrClose">
             <img src={hamburger} alt="menu" />
           </div>
-        ) : (
-          <div>
-            <div className="hamburgOrClose" style={{backgroundColor: '#005397'}}>
-              <img src={close} alt="close" />
-            </div>
-            <FullMenu
-              pose={this.state.closed ? "hide" : "show"}
-              className="dropMenuContainer"
-            >
-              <div className="dropMenu">
-                <div className="dropItem">Vegan Burger</div>
-                <div className="dropItem">Vegan Ice Cream</div>
-                <div className="dropItem">Crumpets</div>
-                <div className="dropItem">Hungarian Goulash</div>
-                <div className="dropItem">Chai, Honey and Oat Milk</div>
-              </div>
-            </FullMenu>
+        ) : null}
+        <div
+          className="dropMenuContainer"
+          style={
+            closed
+              ? { transform: "translateY(-1000px)" }
+              : { transform: "translateY(0)" }
+          }
+        >
+          <div
+            className="hamburgOrClose close"
+          >
+            <img src={close} alt="close" />
           </div>
-        )}
+          <div className="dropMenu">
+            <div className="dropItem">Vegan Burger</div>
+            <div className="dropItem">Vegan Ice Cream</div>
+            <div className="dropItem">Crumpets</div>
+            <div className="dropItem">Hungarian Goulash</div>
+            <div className="dropItem">Chai, Honey and Oat Milk</div>
+          </div>
+        </div>
       </div>
     );
   }
